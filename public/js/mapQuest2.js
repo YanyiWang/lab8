8df6f9cb7579b17ffc751140d61e56f5263c2afc
YanyiWang <yaw125@ucsd.edu>
2019-02-28 11:04:26 -0800

    thirdpartyAPI lab

---
+++ public/js/mapQuest2.js
@@ -0,0 +1,10 @@
+function initMap() {
+	L.mapquest.key = 'tyZ9q9wLAVTmKi68XG43NAFwAtznQd1G';
+	var map = L.mapquest.map('map',{
+		center:[32.88,-117.236],
+		layers:L.mapquest.tileLayer('map'),
+		zoom:12,
+		zoomControl:false
+	});
+	L.marker([32.88,-117.236]).addTo(map);
+}
